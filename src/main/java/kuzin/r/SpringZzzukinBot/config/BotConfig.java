f9dee822b11145252cd1e8aa254e2c853dca6c2b
roman.kuzin <kuzin.r@gmail.com>
2022-11-03 01:51:32 +0300

    Add CustomContainer

--- src/main/java/kuzin/r/SpringZzzukinBot/config/BotConfig.java
+++ src/main/java/kuzin/r/SpringZzzukinBot/config/BotConfig.java
@@ -2,6 +2,7 @@ package kuzin.r.SpringZzzukinBot.config;
 
 import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 
