6223eb95f08f78f9c54a546d1ecd3eaad41a5a96
roman.kuzin <kuzin.r@gmail.com>
2022-11-04 23:51:45 +0300

    Add scheduling weather data update

--- src/main/java/kuzin/r/SpringZzzukinBot/SpringZzzukinBotApplication.java
+++ src/main/java/kuzin/r/SpringZzzukinBot/SpringZzzukinBotApplication.java
@@ -1,14 +1,11 @@
 package kuzin.r.SpringZzzukinBot;
 
-import kuzin.r.SpringZzzukinBot.config.BotConfig;
-import kuzin.r.SpringZzzukinBot.config.BotInitializer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 public class SpringZzzukinBotApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SpringZzzukinBotApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringZzzukinBotApplication.class, args);
+    }
 }
